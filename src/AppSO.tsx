6ebf1d82ced04ef749584996afa358b17993e897
LuisO <89655829+LuisOrescoEOTW@users.noreply.github.com>
2023-09-03 00:20:23 -0300

    comit de cambio

---
+++ src/AppSO.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AppSO = () => {
+  return (
+    <div>AppSO esta bien</div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { AppSO } from './AppSO'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AppSO />
+  </React.StrictMode>,
+)
