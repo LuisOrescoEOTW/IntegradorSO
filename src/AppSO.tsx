f8fba12a00758a2b5c5736f23fbcd6579b44047c
LuisO <89655829+LuisOrescoEOTW@users.noreply.github.com>
2023-09-14 16:21:43 -0300

    +

--- src/AppSO.tsx
+++ src/AppSO.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
 
 export const AppSO = () => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [fileContent, setFileContent] = useState<string>('');
+
+  const handleFileChange = () => {
+    const file = fileInputRef.current?.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const content = event.target?.result as string;
+        setFileContent(content);
+      };
+      reader.readAsText(file);
+    }
+  };
+
   return (
-    <div>AppSO esta bien</div>
-  )
-}
+    <div>
+      <input type="file" ref={fileInputRef} onChange={handleFileChange} />
+      <div>AppSO está bien</div>
+      <div>Mostrar lo que hay: {fileContent}</div>
+    </div>
+  );
+};
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+
+// })
+
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost'
+    }
+  },
+  plugins: [
+    react({
+      include: "**/*.tsx",
+    }),
+  ],
 })
